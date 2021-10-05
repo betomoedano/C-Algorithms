75a1795e496410ef7b1c1f21995c62ee360be608
betomoedano <betomoedano@outlook.com>
2021-10-05 15:22:51 -0700

    Initial Commit

---
+++ C++/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  C++
+//
+//  Created by Alberto Moedano on 10/5/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
