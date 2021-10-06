57eebd92bdbd64698b2b925602d408bebd691f6e
betomoedano <betomoedano@outlook.com>
2021-10-05 21:22:27 -0700

    Encoding algorithm

--- C++/main.cpp
+++ C++/main.cpp
@@ -6,9 +6,44 @@
 //
 
 #include <iostream>
+#include <string>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+using namespace std;
+
+string encoding(string input) {
+    if( input == "" ) {
+        return "the input was null";
+    }
+    string output = "";
+    int counter = 0;
+    
+    for (int i = 1; i <= input.length(); i ++) {
+        if (input[i] == input[i - 1]) {
+            counter ++;
+        } else if (input[i - 1] != 0) {
+            counter ++;
+            output += to_string(counter) + input[i - 1];
+            counter = 0;
+        }
+    }
+    return output;
+}
+
+int main() {
+    //Run length encoding
+    //given this string you have to encode it.
+    
+    //aaaabbccc String example
+    //4a2b3c    Output example
+    
+    string input = "aaaabbccc";
+    
+    cout << encoding("") << endl;
+    cout << encoding("aldhfajklshfsdajlkhjhkhdjhdjhfjhjfhfjd") << endl;
+    cout << encoding("") << endl;
+    cout << encoding("aaaafffffwwwwwwwbbccc") << endl;
+    cout << encoding("asfg") << endl;
+
+  
+  return 0;
 }
